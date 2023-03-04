f41d4e762bcf5ac5b4c85ad3f32cfce51c93463f
Lucas Sentis <lucas.sentis.fuster@students.iaac.net>
2023-03-04 11:41:55 +0800

    V1

--- fetch.py
+++ fetch.py
@@ -7,8 +7,10 @@ def send_to_compute():
     #Credentials
     compute_rhino3d.Util.url = 'http://35.157.191.153/'
     compute_rhino3d.Util.apiKey = 'kXr4&be2ut3rzn'
+   
+   #adjust both name 
     send_to_compute_stream = "https://macad.speckle.xyz/streams/3d0adf77de"
-    gh_definition = "example.ghx"
+    gh_definition = "example.ghx" 
 
     gh_data = open(gh_definition, mode="r", encoding="utf-8-sig").read()
     data_bytes = gh_data.encode("utf-8")
